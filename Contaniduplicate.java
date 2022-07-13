f242d9f5ff342f5a8ca6b45a9529d251c78a5b93
Karthik L <94213422+karthikmsc-008@users.noreply.github.com>
2022-07-13 13:05:51 +0530

    Contain duplicate problem solved using java

---
+++ Contaniduplicate.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+         boolean answer=false;
+        Arrays.sort(nums); //Sort the array because of finding duplicate elements
+        for(int i=0;i<nums.length-1;i++)
+        {
+           
+                if(nums[i]==nums[i+1]) // condition both values or same ot not
+                    answer=true;
+        
+        }
+        return answer;
+    }
+}
